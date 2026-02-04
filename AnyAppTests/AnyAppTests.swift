f8a596bada1c48c66b5707716345e35f6f7d5d37
joaorbrto <jrarrow23@gmail.com>
2026-02-04 18:26:52 -0300

    Initial Commit

---
+++ AnyAppTests/AnyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnyAppTests.swift
+//  AnyAppTests
+//
+//  Created by João Roberto Fernandes Magalhães on 04/02/26.
+//
+
+import Testing
+@testable import AnyApp
+
+struct AnyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
